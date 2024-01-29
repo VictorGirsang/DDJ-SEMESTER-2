31094cdd0aa1c35edaaa3bb5293bbad9774548b7
Victor <140993993+VictorGirsang@users.noreply.github.com>
2024-01-29 13:32:28 +1100

    Add files via upload

---
+++ operator_aritmatika.py
@@ -0,0 +1,16 @@
+x = 5
+y = 3
+#perjumpalahan
+print("penjumlahan 5 + 3 = ", x+y)
+print("penjumlahan 5 - 3 = ", x-y)
+print("penjumlahan 5 * 3 = ", x*y)
+print("penjumlahan 5 / 3 = ", x/y)
+print("pembagian bulat 5 //  3 = ", x//y)
+print("Modulus 5 % 3 = ", x%y)
+print("perpangkatan 5 **() 3 = ", x**y)
+
+
+
+
+
+
